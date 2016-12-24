4fc91e5b9ee12762096d5ccf925f6db7d466a49c
Prakhar tripathi <prakhartripathi.4896@gmail.com>
2016-12-25 01:55:43 +0530

    csc
    csc

--- quote.js
+++ quote.js
@@ -1,5 +1,5 @@
 var quotes = [ 'I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel. Maya Angelou',
-'Whether you think you can or you think you can\’t, you\’re right. Henry Ford',
+'Whether you think you can or you think you can’\t, you’re right. Henry Ford',
 'Perfection is not attainable, but if we chase perfection we can catch excellence. Vince Lombardi',
 'Remember no one can make you feel inferior without your consent. Eleanor Roosevelt',
 'Strive not to be a success, but rather to be of value. Albert Einstein',
@@ -9,6 +9,12 @@ var quotes = [ 'I’ve learned that people will forget what you said, people wil
 ]
 
 function newQuote() {
-  var randomNumber = Math.floor(Math.random() * (quotes.length));
-  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+  randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  document.getElementById('quoteDisplay').innerHTML = randomQuote;
 }
+$(document).ready(function() {
+
+ $(".twitter-share-button").click(function(){
+    $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + randomQuote);
+  });
+});
